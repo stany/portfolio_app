2d0594838836910808ee2f6322746d7346f84fb8
Aleksandr Lossenko <egze@egze-ubuntu.(none)>
2008-10-25 18:01:30 +0200

    all tests use factory_girl

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -9,63 +9,70 @@ class SessionsControllerTest < ActionController::TestCase
   # Then, you can remove it from this and the units test.
   include AuthenticatedTestHelper
 
-  fixtures :users
-
   def test_should_login_and_redirect
-    post :create, :login => 'quentin', :password => 'monkey'
+    u = Factory(:user)
+    post :create, :login => u.login, :password => 'monkey'
     assert session[:user_id]
     assert_response :redirect
   end
 
   def test_should_fail_login_and_not_redirect
-    post :create, :login => 'quentin', :password => 'bad password'
+    u = Factory(:user)
+    post :create, :login => u.login, :password => 'bad password'
     assert_nil session[:user_id]
     assert_response :success
   end
 
   def test_should_logout
-    login_as :quentin
+    u = Factory(:user)
+    login_as u
     get :destroy
     assert_nil session[:user_id]
     assert_response :redirect
   end
 
   def test_should_remember_me
+    u = Factory(:user)
     @request.cookies["auth_token"] = nil
-    post :create, :login => 'quentin', :password => 'monkey', :remember_me => "1"
+    post :create, :login => u.login, :password => 'monkey', :remember_me => "1"
     assert_not_nil @response.cookies["auth_token"]
   end
 
   def test_should_not_remember_me
+    u = Factory(:user)
     @request.cookies["auth_token"] = nil
-    post :create, :login => 'quentin', :password => 'monkey', :remember_me => "0"
+    post :create, :login => u.login, :password => 'monkey', :remember_me => "0"
     puts @response.cookies["auth_token"]
     assert @response.cookies["auth_token"].blank?
   end
   
   def test_should_delete_token_on_logout
-    login_as :quentin
+    user = Factory(:user)
+    login_as user
     get :destroy
     assert @response.cookies["auth_token"].blank?
   end
 
   def test_should_login_with_cookie
-    users(:quentin).remember_me
-    @request.cookies["auth_token"] = cookie_for(:quentin)
+    u = Factory(:user)
+    u.remember_me
+    @request.cookies["auth_token"] = cookie_for(u)
     get :new
     assert @controller.send(:logged_in?)
   end
 
   def test_should_fail_expired_cookie_login
-    users(:quentin).remember_me
-    users(:quentin).update_attribute :remember_token_expires_at, 5.minutes.ago
-    @request.cookies["auth_token"] = cookie_for(:quentin)
+    u = Factory(:user)
+    u.remember_me
+    u.update_attribute :remember_token_expires_at, 5.minutes.ago
+    @request.cookies["auth_token"] = cookie_for(u)
     get :new
     assert !@controller.send(:logged_in?)
   end
 
   def test_should_fail_cookie_login
-    users(:quentin).remember_me
+    u = Factory(:user)
+    u.remember_me
     @request.cookies["auth_token"] = auth_token('invalid_auth_token')
     get :new
     assert !@controller.send(:logged_in?)
@@ -77,6 +84,6 @@ class SessionsControllerTest < ActionController::TestCase
     end
     
     def cookie_for(user)
-      auth_token users(user).remember_token
+      auth_token user.remember_token
     end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,15 +1,9 @@
 require File.dirname(__FILE__) + '/../test_helper'
-require 'users_controller'
-
-# Re-raise errors caught by the controller.
-class UsersController; def rescue_action(e) raise e end; end
 
 class UsersControllerTest < ActionController::TestCase
   # Be sure to include AuthenticatedTestHelper in test/test_helper.rb instead
   # Then, you can remove it from this and the units test.
   include AuthenticatedTestHelper
-
-  fixtures :users
 
   def test_should_allow_signup
     assert_difference 'User.count' do
@@ -59,11 +53,12 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   def test_should_activate_user
-    assert_nil User.authenticate('aaron', 'test')
-    get :activate, :activation_code => users(:aaron).activation_code
+    user = Factory(:user, :login => 'aaron', :activated_at => nil, :activation_code => "qwertz")
+    assert_nil User.authenticate(user.login, 'monkey')
+    get :activate, :activation_code => user.activation_code
     assert_redirected_to login_path
     assert_not_nil flash[:notice]
-    assert_equal users(:aaron), User.authenticate('aaron', 'monkey')
+    assert_equal user, User.authenticate(user.login, 'monkey')
   end
   
   def test_should_not_activate_user_without_key
